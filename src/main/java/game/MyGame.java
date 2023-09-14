0443ee996098c1aef06c9ac32cc5b539b7d87009
mustafabek45 <bekmustafa45@gmail.com>
2023-09-14 12:09:07 +0200

    main method added

---
+++ src/main/java/game/MyGame.java
@@ -0,0 +1,12 @@
+package game;
+
+public class MyGame {
+    public MyGame(String title) {
+    }
+
+    public static void main(String[] args) {
+
+        MyGame screen=new MyGame("My Game");
+    }
+
+}
